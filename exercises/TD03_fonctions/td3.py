a26fadc6cf29b2af2aec461f112ae62693343f5f
Laurédane COUSTILLAS <lauredane.coustillas@ens.uvsq.fr>
2021-11-24 09:42:12 +0100

    td3 application

--- exercises/TD03_fonctions/td3.py
+++ exercises/TD03_fonctions/td3.py
@@ -1,8 +1,7 @@
+temps = (3,23,1,34)
 def tempsEnSeconde(temps):
     """ Renvoie la valeur en seconde de temps donné comme jour, heure, minute, seconde."""
     return temps[3]+60*temps[2]+60*60*temps[1] +temps[0]*(60*60*24)
-    
-temps = (3,23,1,34)
 print(type(temps))
 print( "Le temps en seconde est", (tempsEnSeconde(temps)))
    
@@ -18,10 +17,11 @@ def secondeEnTemps(seconde):
     temps_minute = seconde //60
     temps_seconde = seconde % 60
     return (int(temps_jour),int( temps_heure), int(temps_minute),int(temps_seconde))
-    
 
 temps = secondeEnTemps(100000)
-print(temps[0],"jours",temps[1],"heures",temps[2],"minutes",temps[3],"secondes")
+print("Fonction secondeEnTemps :")
+print(temps[0]," jours ",temps[1]," heures ",temps[2]," minutes ",temps[3]," secondes ")
+
 
 #fonction auxiliaire ici
 
@@ -36,70 +36,72 @@ def afficheTemps(temps):
         str_temps +="."
     
     if temps[1] >1:
-        str_temps += f'{ (temps[1])}heures'
+        str_temps += f'{ (temps[1])} heures'
     elif temps[1]>0 :
-        str_temps += f'{ (temps[0])}heure'
+        str_temps += f'{ (temps[0])} heure'
     if temps[1] > 0 and ( temps[2]>0 or temps[3]>0):
         str_temps +="."
 
     if temps[2] >1:
-        str_temps += f'{ (temps[2])}minutes'
+        str_temps += f'{ (temps[2])} minutes'
     elif temps[2]>0 :
-        str_temps += f'{ (temps[2])}minute'
+        str_temps += f'{ (temps[2])} minute'
     if temps[2] > 0 and temps[3]>0:
         str_temps += "."
 
     if temps[3] >1:
-        str_temps += f'{ (temps[3])}secondes'
+        str_temps += f'{ (temps[3])} secondes'
     elif temps[3]>0 :
-        str_temps += f'{ (temps[3])}seconde'
-    print(str_temps)
+        str_temps += f'{ (temps[3])} seconde'
+    print ("Fonction afficheTemps :")
+    print("Le temps vaut ", (str_temps))
 
 afficheTemps((1,0,14,23))
 
+
 def demandeTemps():
-   jour = int(input ("entrez un temps en jour, heures, minutes et secondes"))
-   while jour < 0:
-       jour = int(input("le nbr d'heures doit être compris entre 0 et 24"))
-     
-    heure = int(input("Entrez un nombre d'heures") 
-    while heure < 0:
-
-
-    minute =
-   while minute <0 :
-    seconde =
-    seconde
-
+    jour = 0
+    jour = int(input("Quel est le nombre de jour de votre valeur ?"))
+    while jour > 365 or jour < 0:
+        jour = int(input("Il faut que le nombre de jour soit cohérent."))
+    heure = 0
+    heure = int(input("Quel est le nombre d'heure de votre temps?"))
+    while heure > 24 or jour < 0:
+        heure = int(input("Il faut que le nombre d'heure soit cohérent."))
+    minute = 0
+    minute = int(input("Quel est le nombre de minute de votre valeur ?"))
+    while minute > 60 or minute < 0:
+        minute = int(input("Il faut que le nombre de minute soit cohérent."))
+    seconde = 0
+    seconde = int(input("Quel est votre nombre de seconde dans votre temps ?"))
+    while seconde > 60 or seconde < 0:
+        seconde = int(input("Il faut que le nombre de seconde soit cohérent."))
+    return (jour, heure, minute, seconde)
+print ("Fonction demandeTemps :")
 afficheTemps(demandeTemps())
 
-
+print ("Fonction sommeTemps :")
 def sommeTemps(temps1,temps2):
-    
+    temps1 = (2,3,4,25)
+    temps2 = (5,22,57,1)
+    print (temps1, temps2)
+    return sommeTemps(temps1, temps2)
 
-def sommeTemps((2,3,4,25),(5,22,57,1))
-    seconde_t1 = tempsEnSeconde (temps1)
-    seconde_t2 = tempsEnSeconde (temps2)
-    return secondeEnTemps (seconde_t1 + seconde_t2)
 
 
 def proportionTemps(temps,proportion):
     seconde_t = tempsEnSeconde( temps)
-
+    print ("Fonction proportionTemps :")
     return secondeEnTemps( seconde_t * proportion)
 
 
-afficheTemps(proportionTemps((2,0,36,0),0.2))
-
-afficheTemps((1,0,14,23))
-
-def proportionTemps(temps: list, proportion: int):
+def proportionTemps(temps, proportion):
     return secondeEnTemps((tempsEnSeconde(temps) * (proportion / 100)))
-# resultat = proportionTemps((2, 0, 36, 0), 0.2)
-# afficheTemps(resultat)
+resultat = proportionTemps((2, 0, 36, 0), 0.2)
+afficheTemps(resultat)
 
-
-def tempsEnDate(temps: list):
+print ("Fonction tempsEnDate:")
+def tempsEnDate(temps):
     annees = tempsEnSeconde(temps) // (60 * 60 * 24 * 365)
     temps = tempsEnSeconde(temps) % (60 * 60 * 24 * 365)
     jours = temps // (60 * 60 * 24)
@@ -111,12 +113,12 @@ def tempsEnDate(temps: list):
     secondes = temps
     return (annees, jours, heures, minutes, secondes)
 
-
-def secondeEnDate(secondes: int):
+print ("Fonction secondeEnDate:")
+def secondeEnDate(secondes):
     return tempsEnDate(secondeEnTemps(secondes))
 
-
-def afficheDate(date=secondeEnDate(time.time())):
+print ("Fonction afficheDate:")
+def afficheDate(date = -1):
     if date[0] != 0 and date[0] > 1:
         print(date[0], "années", end=" ")
     elif date[0] == 1:
@@ -139,18 +141,12 @@ def afficheDate(date=secondeEnDate(time.time())):
         print(date[4], "seconde", end=" ")
 
 
-# temps = secondeEnTemps(1000000000)
-# afficheTemps(temps)
-# afficheDate(tempsEnDate(temps))
+temps = secondeEnTemps(1000000000)
+afficheTemps(temps)
+afficheDate(tempsEnDate(temps))
 
 
-# import time
-# print(secondeEnTemps(time.time()))
-# afficheDate(tempsEnDate(secondeEnTemps(time.time())))
-# secondeEnTemps(time.time())
-# time.gmtime()
-
-def nombreBisextile(jour: int):
+def nombreBisextile(jour):
     temps = (int(jour), 0, 0, 0)
     date = tempsEnDate(secondeEnTemps(tempsEnSeconde(temps)))
     annee = date[0]
@@ -161,24 +157,22 @@ def nombreBisextile(jour: int):
         annee += -1
     return compteur
 
+print("Le nombre est:", nombreBisextile(200000))
 
-# print(nombreBisextile(200000))
 
-
-def tempsEnDateBisextile(temps: list):
+def tempsEnDateBisextile(temps):
     jours = temps[0]
     jours += nombreBisextile(jours)
     date = (0, jours, temps[1], temps[2], temps[3])
     return date
 
 
-# temps = secondeEnTemps(1000000000)
-# afficheTemps(temps)
-# afficheDate(tempsEnDateBisextile(temps))
-# afficheDate()
+temps = secondeEnTemps(1000000000)
+afficheTemps(temps)
+afficheDate(tempsEnDateBisextile(temps))
 
 
-def verifie(horaires: list):
+def verifie(horaires):
     sommeHoraires = 0
     for elem in horaires:
         print("L'employé a fait", elem[1] + 24 * elem[0], "heures dans la semaine.")
@@ -187,8 +181,7 @@ def verifie(horaires: list):
             return print("Les horaires limites ont été dépassés.", "Il a fait", sommeHoraires // 3600, "heures dans le mois.")
     return print("Les horaires limites ont été respectés.", "Il a fait", sommeHoraires // 3600, "heures dans le mois.")
 
-
-# horaires = [[1, 2, 39, 34], [0, 3, 9, 4], [0, 2, 39, 51], [0, 1, 13, 46]]
-# verifie(horaires)
+horaires = [[1, 2, 39, 34], [0, 3, 9, 4], [0, 2, 39, 51], [0, 1, 13, 46]]
+verifie(horaires)
 
 
